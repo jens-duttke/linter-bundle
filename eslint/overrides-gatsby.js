af7a943a1a4d84973c1297b3c9c4377f812a5ea1
Jens Duttke <github@duttke.de>
2021-01-09 12:20:32 +0100

    Add configuration for gatsby

---
+++ eslint/overrides-gatsby.js
@@ -0,0 +1,36 @@
+module.exports = {
+	files: ['*.tsx'],
+	settings: {
+		linkComponents: [
+			{ name: 'Link', linkAttribute: 'to' }
+		]
+	},
+	rules: {
+		/**
+		 * @typescript-eslint Rules
+		 * @see https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules
+		 */
+		'@typescript-eslint/no-confusing-void-expression': 'off', // @todo Conflicts with graphql-template strings
+
+		/**
+		 * eslint-plugin-unicorn Rules
+		 * @see https://github.com/sindresorhus/eslint-plugin-unicorn
+		 */
+		'unicorn/filename-case': ['error', {
+			cases: {
+				camelCase: true,
+				pascalCase: true
+			},
+			ignore: [
+				/([A-Za-z0-9]?[a-z0-9]+)*[A-Z]{2,4}([A-Za-z0-9]?[a-z0-9]+)*/u, // Up to 4 Characters can be upper-case in a row (e.g. in `prepareDOM` or `SVGIcon`)
+				'gatsby-browser.js'
+			]
+		}],
+		'unicorn/string-content': ['error', {
+			patterns: {
+				// '\\.\\.\\.': '…', // @todo Does not support graphql-template strings. Report that as bug!
+				'->': '→'
+			}
+		}],
+	}
+};
