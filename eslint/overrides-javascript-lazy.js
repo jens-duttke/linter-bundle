3df0ab6fcce6da57406b315f21f01f831b1cfc4f
Jens Duttke <github@duttke.de>
2021-01-23 01:13:08 +0100

    Disable no-unsafe-[call/member-access/return] for JS files

---
+++ eslint/overrides-javascript-lazy.js
@@ -0,0 +1,11 @@
+const overridesJavascript = require('overrides.javascript');
+
+/**
+ * @typescript-eslint
+ * @see https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules
+ */
+overridesJavaScript.overrides[0].rules['@typescript-eslint/no-unsafe-call'] = 'off';
+overridesJavaScript.overrides[0].rules['@typescript-eslint/no-unsafe-member-access'] = 'off';
+overridesJavaScript.overrides[0].rules['@typescript-eslint/no-unsafe-return'] = 'off';
+
+module.exports = overridesJavaScript;
