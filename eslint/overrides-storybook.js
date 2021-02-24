85f07a9311dfadf8ed684b4dae02584c59619dd2
Jens Duttke <github@duttke.de>
2021-02-24 13:03:48 +0100

    Fix import of "path"

--- eslint/overrides-storybook.js
+++ eslint/overrides-storybook.js
@@ -17,7 +17,22 @@ module.exports = {
 				 * eslint-plugin-import
 				 * @see https://github.com/benmosher/eslint-plugin-import
 				 */
-				'import/no-nodejs-modules': 'off'
+				'import/no-nodejs-modules': 'off',
+
+				/**
+				 * eslint-plugin-unicorn
+				 * @see https://github.com/sindresorhus/eslint-plugin-unicorn
+				 */
+				'unicorn/import-style': ['error', {
+					styles: {
+						path: {
+							unassigned: false,
+							default: false,
+							namespace: true,
+							named: true
+						}
+					}
+				}]
 			}
 		}
 	]
