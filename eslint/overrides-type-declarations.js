4ac4b2764d1c7f0f35fb23c451c0fa23b1438b3c
Jens Duttke <github@duttke.de>
2021-03-03 17:45:07 +0100

    Remove workaround, because of fix in @typescript-eslint 4.16.0

--- eslint/overrides-type-declarations.js
+++ eslint/overrides-type-declarations.js
@@ -11,12 +11,6 @@ module.exports = {
 				'one-var': 'off',
 
 				/**
-				 * @typescript-eslint
-				 * @see https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules
-				 */
-				'@typescript-eslint/no-unused-vars': 'off', // This rule reports interfaces as unused vars in d.ts files.
-
-				/**
 				 * eslint-plugin-import
 				 * @see https://github.com/benmosher/eslint-plugin-import
 				 */
