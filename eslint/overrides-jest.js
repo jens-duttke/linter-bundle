257f4be85a21a4a532a630bde37984015a6c9ba0
Jens Duttke <github@duttke.de>
2021-06-14 16:25:57 +0200

    Don't apply rules for [jt]sx files

--- eslint/overrides-jest.js
+++ eslint/overrides-jest.js
@@ -5,7 +5,7 @@
 module.exports = {
 	overrides: [
 		{
-			files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+			files: ['**/__tests__/**/*.[jt]s', '**/?(*.)+(spec|test).[jt]s'],
 			env: {
 				jest: true
 			},
