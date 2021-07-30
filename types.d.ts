2d04372503bd6e6e5228621ad0f1dffb3d085d3e
Jens Duttke <github@duttke.de>
2021-07-30 07:44:55 +0200

    Add comment about the changes required for TS 4.3.5

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,15 @@
+/**
+ *
+ * As of TypeScript 4.3.5, assigning a property to `globalThis` is less intuitive.
+ *
+ * There are two ways, which all require to disable TSC-error reporting and a couple of linter warning.
+ *
+ * If the way below doesn't work anymore, this is an alternative:
+ * export declare global {
+ *
+ * @see https://stackoverflow.com/a/68452689/4449804
+ */
+
 // @ts-expect-error -- False-positive
 declare namespace globalThis {
 	// eslint-disable-next-line no-inner-declarations, vars-on-top, no-var -- False-positives
