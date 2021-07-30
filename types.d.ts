f439487360bb8c2525b07f38fba3ec809266905c
Jens Duttke <github@duttke.de>
2021-07-30 07:32:42 +0200

    Fix typings to fullfill new TypeScript requirements

--- types.d.ts
+++ types.d.ts
@@ -1,29 +1,29 @@
-declare namespace NodeJS {
-	interface Global {
-		linterBundleSettings?: {
-			overrides?: {
-				general?: {
-					'no-restricted-globals'?: {
-						additionalRestictions?: string[];
-					};
-					'no-restricted-properties'?: {
-						additionalRestictions?: string[];
-					};
-					'no-restricted-syntax'?: {
-						additionalRestictions?: string[];
-					};
-					'import/order'?: {
-						additionalExternalPatterns?: string[];
-					};
+// @ts-expect-error -- False-positive
+declare namespace globalThis {
+	// eslint-disable-next-line no-inner-declarations, vars-on-top, no-var -- False-positives
+	var linterBundleSettings: {
+		readonly overrides?: {
+			readonly general?: {
+				readonly 'no-restricted-globals'?: {
+					readonly additionalRestictions?: string[];
 				};
-				react?: {
-					'react/forbid-component-props'?: {
-						allowClassNameFor?: string[];
-						allowStyleFor?: string[];
-					};
+				readonly 'no-restricted-properties'?: {
+					readonly additionalRestictions?: string[];
+				};
+				readonly 'no-restricted-syntax'?: {
+					readonly additionalRestictions?: string[];
+				};
+				readonly 'import/order'?: {
+					readonly additionalExternalPatterns?: string[];
 				};
 			};
-			patternPrefix?: string;
+			readonly react?: {
+				readonly 'react/forbid-component-props'?: {
+					readonly allowClassNameFor?: string[];
+					readonly allowStyleFor?: string[];
+				};
+			};
 		};
-	}
+		readonly patternPrefix?: string;
+	} | undefined;
 }
