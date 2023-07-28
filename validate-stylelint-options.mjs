613023f47043a19c2e9a0dadacd555cc23ee11c4
Jens <github@duttke.de>
2023-07-28 15:42:22 +0200

    Add additional tests

--- validate-stylelint-options.mjs
+++ validate-stylelint-options.mjs
@@ -16,6 +16,20 @@ process.exitCode = await (async () => {
 	const tempFilePath = path.join(folder, 'tmp.scss');
 
 	fs.writeFileSync(tempFilePath, `// Some test code
+$box-shadow-color: #fff; // stylelint-disable-line color-no-hex -- The initial value must be a hex-color
+$border-bottom: 10px;
+$box-shadow-size: 2px;
+
+@function custom-makelongshadow ($color, $size) {
+	$val: 5px 0 0 $size $color !default;
+
+	@for $i from 6 through 2 {
+		$val: #{$val}, ($i * 1px) 0 0 $size #{$color};
+	}
+
+	@return $val;
+}
+
 @property --color {
 	syntax: "<color>";
 	inherits: false;
@@ -23,7 +37,17 @@ process.exitCode = await (async () => {
 }
 
 .test, { // stylelint-disable-line plugin/stylelint-selector-no-empty -- Ensure this throws an error which can be suppressed here.
+	grid-template-areas:
+		(
+			"a1"
+			"a2"
+			"a3"
+		);
+
 	border: 1px solid var(--color);
+	border-bottom: 1px dotted $border-bottom;
+
+	box-shadow: custom-makelongshadow($box-shadow-color, $box-shadow-size);
 
 	color: var(--color);
 }
