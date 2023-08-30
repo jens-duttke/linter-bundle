a1859618eacacaf3b3aebd71e48afed9600be4d7
Jens <github@duttke.de>
2023-08-30 19:17:02 +0200

    Extract gitFiles functionality

---
+++ helper/get-git-files.js
@@ -0,0 +1,51 @@
+/**
+ * @file Handling of changed files from Git.
+ */
+
+const { runProcess } = require('./run-process.js');
+
+/** @typedef {import('./run-process').ProcessResult} ProcessResult */
+
+/** @type {{ diff: Promise<ProcessResult>; modified: Promise<ProcessResult>; deleted: Promise<ProcessResult>; } | undefined} */
+let gitFilesProcessPromise;
+/** @type {string[] | undefined} */
+let gitFiles;
+
+/**
+ * Returns a list of changed files based on Git.
+ *
+ * @returns {Promise<string[]>} The list of changed files.
+ */
+async function getGitFiles () {
+	if (!gitFilesProcessPromise) {
+		gitFilesProcessPromise = {
+			// Returns changed files, also stashed and committed
+			diff: runProcess('git diff --name-only -z @{upstream}'),
+			// Returns unstashed files (including deleted)
+			modified: runProcess('git ls-files -o -m --exclude-standard --full-name --deduplicate -z'),
+			// Returns unstashed, deleted files - @todo Is there a way to also get a list of deleted stashed/committed files?
+			deleted: runProcess('git ls-files -d --exclude-standard --full-name --deduplicate -z')
+		};
+	}
+
+	const gitProcessResult = {
+		diff: await gitFilesProcessPromise.diff,
+		modified: await gitFilesProcessPromise.modified,
+		deleted: await gitFilesProcessPromise.deleted
+	};
+
+	if (!gitFiles) {
+		const deletedFiles = gitProcessResult.deleted.stdout.trim().split('\0');
+
+		gitFiles = [
+			...gitProcessResult.diff.stdout.trim().split('\0'),
+			...gitProcessResult.modified.stdout.trim().split('\0')
+		].filter((file, index, self) => !deletedFiles.includes(file) && self.indexOf(file) === index);
+	}
+
+	return gitFiles;
+}
+
+module.exports = {
+	getGitFiles
+};
--- lint.js
+++ lint.js
@@ -29,41 +29,8 @@ void (async () => {
 		return;
 	}
 
-	/** @type {{ diff: Promise<ProcessResult>; modified: Promise<ProcessResult>; deleted: Promise<ProcessResult>; } | undefined} */
-	let gitFilesProcessPromise;
-	/** @type {string[] | undefined} */
-	let gitFiles;
-
 	/** @type {Job[]} */
-	const jobs = await Promise.all(getTasksToRun(process.argv.splice(2)).map(async ({ taskName, config }) => {
-		if (config['git']) {
-			if (!gitFilesProcessPromise) {
-				gitFilesProcessPromise = {
-					// Returns changed files, also stashed and committed
-					diff: runProcess('git diff --name-only -z @{upstream}'),
-					// Returns unstashed files (including deleted)
-					modified: runProcess('git ls-files -o -m --exclude-standard --full-name --deduplicate -z'),
-					// Returns unstashed, deleted files - @todo Is there a way to also get a list of deleted stashed/committed files?
-					deleted: runProcess('git ls-files -d --exclude-standard --full-name --deduplicate -z')
-				};
-			}
-
-			const gitProcessResult = {
-				diff: await gitFilesProcessPromise.diff,
-				modified: await gitFilesProcessPromise.modified,
-				deleted: await gitFilesProcessPromise.deleted
-			};
-
-			if (!gitFiles) {
-				const deletedFiles = gitProcessResult.deleted.stdout.trim().split('\0');
-
-				gitFiles = [
-					...gitProcessResult.diff.stdout.trim().split('\0'),
-					...gitProcessResult.modified.stdout.trim().split('\0')
-				].filter((file, index, self) => !deletedFiles.includes(file) && self.indexOf(file) === index);
-			}
-		}
-
+	const jobs = await Promise.all(getTasksToRun(process.argv.splice(2)).map(async ({ taskName, taskConfig }) => {
 		switch (taskName) {
 			case 'tsc':
 				// eslint-disable-next-line @typescript-eslint/no-dynamic-delete -- This is not a valid `tsc` property, so  we need to remove it.
