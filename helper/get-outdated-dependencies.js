a33773d602d8fb907806ea5505a99e3bdb54c0ed
Jens <github@duttke.de>
2023-09-03 13:00:02 +0200

    Change function name to better reflect the functionality

---
+++ helper/get-outdated-dependencies.js
@@ -0,0 +1,48 @@
+/**
+ * @file Ensures that the installed dependencies of the project matches to the versions used by the linter-bundle.
+ *
+ * @see https://docs.npmjs.com/cli/v8/configuring-npm/package-json#overrides
+ * @see https://classic.yarnpkg.com/en/docs/selective-version-resolutions/
+ */
+
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/** @typedef {{ name: string; configuredVersion: string; expectedVersion: string; }} Dependency */
+
+/**
+ * Detects if installed versions of dependencies don't match to the required dependencies.
+ *
+ * @public
+ * @returns {Promise<Dependency[]>} An array of missing overrides (=wrong versions).
+ */
+export async function getOutdatedDependencies () {
+	const linterBundleDependencies = JSON.parse(await fs.readFile(path.resolve(dirname, '../package.json'), 'utf8')).dependencies;
+
+	const outdatedDependencies = await Promise.all(Object.entries(linterBundleDependencies).map(async ([name, expectedVersion]) => {
+		let dependencyPackageJson;
+
+		try {
+			dependencyPackageJson = JSON.parse(await fs.readFile(path.join(process.cwd(), 'node_modules', name, 'package.json'), 'utf8'));
+		}
+		catch {
+			// If the file does not exist, we ignore it, because in this case it's most likely a linter-bundle-only dependency.
+			return;
+		}
+
+		if (dependencyPackageJson.version !== expectedVersion) {
+			return {
+				name,
+				configuredVersion: dependencyPackageJson.version,
+				expectedVersion
+			};
+		}
+
+		return;
+	}));
+
+	return /** @type {Dependency[]} */(outdatedDependencies.filter((dependency) => dependency !== undefined));
+}
---
+++ helper/get-outdated-overrides.js
@@ -0,0 +1,62 @@
+/**
+ * @file Ensures that the "overrides" and "resolutions" of the project where linter-bundle is used, match to the versions used in the linter-bundle itself.
+ *
+ * @see https://docs.npmjs.com/cli/v8/configuring-npm/package-json#overrides
+ * @see https://classic.yarnpkg.com/en/docs/selective-version-resolutions/
+ */
+
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/** @typedef {{ name: string; configuredVersion: string; expectedVersion: string; }} Dependency */
+
+/**
+ * Detects outdated "overrides"/"resolutions" dependencies.
+ *
+ * @public
+ * @returns {Promise<{ overrides: Dependency[]; resolutions: Dependency[]; }>} Either the input array, or an empty array, if the input array is not an array.
+ */
+export async function getOutdatedOverrides () {
+	const linterBundleDependencies = JSON.parse(await fs.readFile(path.resolve(dirname, '../package.json'), 'utf8')).dependencies;
+	const projectPackageJson = JSON.parse(await fs.readFile(path.join(process.cwd(), 'package.json'), 'utf8'));
+
+	const overrides = [];
+	const resolutions = [];
+
+	if (typeof projectPackageJson.overrides === 'object' && projectPackageJson.overrides !== null) {
+		for (const [name, version] of Object.entries(projectPackageJson.overrides)) {
+			if (!(name in linterBundleDependencies)) {
+				continue;
+			}
+
+			if (version !== linterBundleDependencies[name]) {
+				overrides.push({
+					name,
+					configuredVersion: version,
+					expectedVersion: linterBundleDependencies[name]
+				});
+			}
+		}
+	}
+
+	if (typeof projectPackageJson.resolutions === 'object' && projectPackageJson.resolutions !== null) {
+		for (const [name, version] of Object.entries(projectPackageJson.resolutions)) {
+			if (!(name in linterBundleDependencies)) {
+				continue;
+			}
+
+			if (version !== linterBundleDependencies[name]) {
+				resolutions.push({
+					name,
+					configuredVersion: version,
+					expectedVersion: linterBundleDependencies[name]
+				});
+			}
+		}
+	}
+
+	return { overrides, resolutions };
+}
