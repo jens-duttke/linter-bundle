2b7a28d54490116c3bb074deab342cfae6f67b19
Jens <github@duttke.de>
2025-03-15 11:50:25 +0100

    Activate all rules to find issues early

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,28 @@
  * @file Configuration used for linting the linter-bundle.
  */
 
+import gatsbyConfig from './eslint/gatsby.mjs';
 import javascriptLazyConfig from './eslint/javascript-lazy.mjs';
+import javascriptConfig from './eslint/javascript.mjs';
+import jestConfig from './eslint/jest.mjs';
 import jsdocConfig from './eslint/jsdoc.mjs';
+import reactConfig from './eslint/react.mjs';
+import storybookConfig from './eslint/storybook.mjs';
+import typeDeclarationsConfig from './eslint/type-declarations.mjs';
+import workerConfig from './eslint/worker.mjs';
 import eslintConfig from './eslint.mjs';
 
 export default [
 	...eslintConfig,
+	...gatsbyConfig,
+	...javascriptConfig,
 	...javascriptLazyConfig,
+	...jestConfig,
 	...jsdocConfig,
+	...reactConfig,
+	...storybookConfig,
+	...typeDeclarationsConfig,
+	...workerConfig,
 	{
 		files: [
 			'eslint.mjs',
